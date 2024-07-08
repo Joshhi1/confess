34edd9fe6dea2150e4e662ae27a4da8833e2993d
Nico’s hidden source codes <157768835+Joshhi1@users.noreply.github.com>
2024-07-08 21:30:19 +0800

    Update __init__.py

--- peysbuk/__init__.py
+++ peysbuk/__init__.py
@@ -1,14 +1,19 @@
-from flask import Flask
+# peysbuk/__init__.py
+
+from flask import Flask, render_template
 
 def createApp():
-  app = Flask(__name__)
-  app.secret_key = 'Lorem Ipsum Loror Sit Emit'
-  
-  from .views import view
-  app.register_blueprint(view, url_prefix='/')
-  from .auth import auth
-  app.register_blueprint(auth, url_prefix='/')
-  @app.errorhandler(404)
-  def page_not_found(e):
-    return render_template('404.html'),404
-  return app
+    app = Flask(__name__)
+    app.secret_key = 'Lorem Ipsum Loror Sit Emit'
+    
+    from .views import view
+    app.register_blueprint(view, url_prefix='/')
+    
+    from .auth import auth
+    app.register_blueprint(auth, url_prefix='/')
+    
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return render_template('404.html'), 404
+    
+    return app
